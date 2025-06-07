62b19919073260ca211a027fabaa9e08ab68413a
SirTaxu <151969050+SirTaxu@users.noreply.github.com>
2025-06-07 01:30:22 -0300

    Update input.h

--- src/Input/input.h
+++ src/Input/input.h
@@ -1,42 +1,12 @@
 //Declara funciones para inicializar y manejar la entrada del usuario (teclado, eventos SDL).
+#include "input.c"
+#ifndef INPUT_H
+#define INPUT_H
 
-void Movement(){
-	if (jugador.arriba == true)
-	{
-		jugador.posY -= jugador.velocidad / 100.0;
-	}
-	if (jugador.abajo)
-	{
-		jugador.posY += jugador.velocidad / 100.0;
-	}
 
-	if (jugador.special == true){
-		posXOriginal = jugador.posX; // Guardar la posición original antes de moverse
-		jugador.posX += jugador.velocidad / 10.0;
-		volver = true;
-	}
-	if (volver == true) {
-		// Volver suavemente hacia la posición original
-		if (jugador.posX != posXOriginal){
-			jugador.posX -= jugador.velocidad / 2.0;
-			jugador.posX = posXOriginal;
-			volver = false;
-		}
-		else
-		{
-			volver = false; // Ya está en su lugar
-		}
-	}
+onKeyUp();
+onKeyDown();
+processInput();
+Movement();
 
-	if (IA.arriba == true)
-	{
-		IA.posY -= IA.velocidad / 100.0;
-	}
-	if (IA.abajo)
-	{
-		IA.posY += IA.velocidad / 100.0;
-	}
-
-	ball.posX += ball.velocidadX / 100;
-	ball.posY += ball.velocidadY / 100;
-}
+#endif // INPUT_H
