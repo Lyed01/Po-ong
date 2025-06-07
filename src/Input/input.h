8275e69fabb6224db6da12a0d834950e650647a9
SirTaxu <151969050+SirTaxu@users.noreply.github.com>
2025-06-07 01:27:11 -0300

    Update input.h

--- src/Input/input.h
+++ src/Input/input.h
@@ -1,30 +1,42 @@
 //Declara funciones para inicializar y manejar la entrada del usuario (teclado, eventos SDL).
 
-	case SDLK_RIGHT:
-		jugador.special = true;
-		break;
+void Movement(){
+	if (jugador.arriba == true)
+	{
+		jugador.posY -= jugador.velocidad / 100.0;
+	}
+	if (jugador.abajo)
+	{
+		jugador.posY += jugador.velocidad / 100.0;
+	}
 
-if (jugador.special == true)
-{
-	posXOriginal = jugador.posX; // Guardar la posición original antes de moverse
-	jugador.posX += jugador.velocidad / 10.0;
-	volver = true;
-}
-if (volver == true) {
+	if (jugador.special == true){
+		posXOriginal = jugador.posX; // Guardar la posición original antes de moverse
+		jugador.posX += jugador.velocidad / 10.0;
+		volver = true;
+	}
+	if (volver == true) {
 		// Volver suavemente hacia la posición original
-		if (jugador.posX > posXOriginal)
-		{
+		if (jugador.posX != posXOriginal){
 			jugador.posX -= jugador.velocidad / 2.0;
-
-			// Evitar pasarse del punto original
-			if (jugador.posX <= posXOriginal)
-			{
-				jugador.posX = posXOriginal;
-				volver = false;
-			}
+			jugador.posX = posXOriginal;
+			volver = false;
 		}
 		else
 		{
 			volver = false; // Ya está en su lugar
 		}
 	}
+
+	if (IA.arriba == true)
+	{
+		IA.posY -= IA.velocidad / 100.0;
+	}
+	if (IA.abajo)
+	{
+		IA.posY += IA.velocidad / 100.0;
+	}
+
+	ball.posX += ball.velocidadX / 100;
+	ball.posY += ball.velocidadY / 100;
+}
