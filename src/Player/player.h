05a9dc482c2aafadea34315691fd0c47091e24e4
SirTaxu <151969050+SirTaxu@users.noreply.github.com>
2025-06-07 01:22:57 -0300

    Update player.h

--- src/Player/player.h
+++ src/Player/player.h
@@ -12,19 +12,19 @@ typedef struct {
     float velocidad;
     bool arriba;
     bool abajo;
-    bool special;
+    bool special = false;
 } Player;
 
 // Inicializa un jugador
-void player_init(Player* player, float x, float y);
+//void player_init(Player* player, float x, float y);
 
 // Mueve al jugador hacia arriba
-void player_move_up(Player* player, float deltaTime);
+//void player_move_up(Player* player, float deltaTime);
 
 // Mueve al jugador hacia abajo
-void player_move_down(Player* player, float deltaTime);
+//void player_move_down(Player* player, float deltaTime);
 
 // Dibuja al jugador (esto lo puedes conectar con tu sistema de renderizado)
-void player_draw(Player* player);
+//void player_draw(Player* player);
 
 #endif // ?? Final del include guard
