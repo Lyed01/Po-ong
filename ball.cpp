f568acc5b7d82a3971973a96cc07b3f29194bd17
SirTaxu <151969050+SirTaxu@users.noreply.github.com>
2025-06-13 15:32:45 -0300

    Update ball.cpp

--- ball.cpp
+++ ball.cpp
@@ -3,14 +3,31 @@
 #include <SDL3/SDL_main.h>
 #include "ball.h"
 
-void checkCollisions(Ball* ball, int ventanaAncho, int ventanaAlto){
-    if (ball.posY > ventanaAlto)
+void checkCollisionsWall(Ball* ball, int ventanaAncho, int ventanaAlto){
+    if ((ball.posY > ventanaAlto) || (ball.posY < 0 ))
     {
         ball.velocidadY *= -1;
     }
-   
-    if (ball.posY < 0)
-    {
-        ball.velocidadY *= -1;
+    if ((bal.posX > VentanaAncho) || (ball.posX < 0)){
+        //Funcion para sumar punto y reiniciar el tablero
     }
+}
+
+void checkCollisionsPlayer(Ball* ball, Player* player, Player* ia){
+    if (ball.posX > (player.posX && player.w){
+        break
+    }
+    if (ball.posY > (player.posY && player.h){
+        break
+    }
+    if (ball.posX > (player.posX && player.w){
+        break
+    }
+    if (ball.posX > (player.posX && player.w){
+        break
+    }
+    else{
+        ball.velocidadX *=-1;
+    }
+    
 }
