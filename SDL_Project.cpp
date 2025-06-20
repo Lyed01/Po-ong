f4fec2115861e687910c8cf59f7ee261ce1b3d39
SirTaxu <151969050+SirTaxu@users.noreply.github.com>
2025-06-20 13:24:46 -0300

    Add files via upload

---
+++ SDL_Project.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include "Game/game.h"
+
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	runGame();
+	return 0;
+}
+
+// input.c identificadores no declarados... player, ball e ia, hay que ver cómo resolver eso
+
+/* Posibles archivos redundantes:
+*	-Player.c 
+*	-ball.c
+*/
+
+
+
+
+
+
+
+	
